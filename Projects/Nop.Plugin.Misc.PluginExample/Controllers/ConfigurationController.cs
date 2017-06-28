d6c17e5088388d9e13e47fbdeaf880c9609df4cd
Manoj Kulkarni <manoj.kulkarni.3011@gmail.com>
2017-06-28 19:06:14 +0530

    Add project files.

---
+++ Projects/Nop.Plugin.Misc.PluginExample/Controllers/ConfigurationController.cs
@@ -0,0 +1,14 @@
+﻿//using Nop.Web.Framework.Controllers;
+
+namespace Nop.Plugin.Misc.PluginExample.Controllers
+{
+    public class ConfigurationController // : BasePluginController
+    {
+        //[AdminAuthorize]
+        //[ChildActionOnly]
+        //public ActionResult Configure()
+        //{
+        //    return View("~/Plugins/Nop.Plugin.Misc.PluginExample/Views/Configure.cshtml", model);
+        //}
+    }
+}
---
+++ Projects/Nop.Plugin.Misc.PluginExample/MiscPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Routing;
+//using Nop.Core.Plugins;
+//using Nop.Services.Common;
+
+namespace Nop.Plugin.Misc.PluginExample
+{
+    public class MiscPlugin // : BasePlugin, IMiscPlugin
+    {
+        public void GetConfigurationRoute(out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            actionName = "Configure";
+            controllerName = "Configuration";
+            routeValues = new RouteValueDictionary { { "Namespaces", "Nop.Plugin.Misc.PluginExample.Controllers" }, { "area", null } };
+        }
+    }
+}
